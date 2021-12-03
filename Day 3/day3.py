6e7d1b9d77432d777d09ffb4bdca7198e0ad44db
Vatsav Sethupathi <vatsav14@outlook.com>
2021-12-03 16:49:49 -0700

    Very bad solution for part 2. Can be fixed but too lazy

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,8 +1,8 @@
 with open("input.txt", "r") as file:
-    data = file.readlines()
+    datas = file.readlines()
 
-def part1():
-    size = len(data[0])
+def part1(data):
+    size = len(datas[0])
     zero = list()
     one = list()
     for each in range(size-1):
@@ -17,6 +17,7 @@ def part1():
                 one[i] += 1
             i += 1
     i = 0
+    '''
     gamma = ""
     epsilon = ""
     while i < len(zero):
@@ -27,7 +28,50 @@ def part1():
             gamma += "1"
             epsilon += "0"
         i += 1
-    print(gamma)
-    print(epsilon)
+    '''
+    return (zero, one)
 
-part1()
+def part2():
+    i = 0
+    oxy = datas
+    zero, one = part1(datas)
+    length = len(zero)
+    while i < length:
+        temp1 = []
+        #print("Zero:", zero[i], "| One:", one[i])
+        for each in oxy:
+            if (zero[i] > one[i]) and (each[i] == '0'):
+                temp1.append(each.strip())
+            elif (one[i] > zero[i]) and (each[i] == '1'):
+                temp1.append(each.strip())
+            elif (one[i] == zero[i]) and (each[i] == '1'):
+                print("Here")
+                temp1.append(each.strip())
+        oxy = temp1
+        if len(oxy) == 1:
+            break
+        zero, one = part1(oxy)
+        i += 1
+
+    co2 = datas
+    zero, one = part1(datas)
+    i = 0
+    while i < length:
+        temp = []
+        for each in co2:
+            if (zero[i] < one[i]) and (each[i] == '0'):
+                temp.append(each.strip())
+            elif (one[i] < zero[i]) and (each[i] == '1'):
+                temp.append(each.strip())
+            elif (one[i] == zero[i]) and (each[i] == '0'):
+                temp.append(each.strip())
+        co2 = temp
+        if len(co2) == 1:
+            break
+        i += 1
+        zero, one = part1(co2)
+
+    print(oxy)
+    print(co2)
+
+part2()
