fe729a5fd1312ecffcc691f04a9b7a2e3b506e17
Vatsav Sethupathi <vatsav14@outlook.com>
2021-12-06 20:09:30 -0700

    Part 2 so much faster than part 1. Shame

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -3,20 +3,44 @@ with open("input.txt", "r") as file:
 
 def part1(days):
     state = data[0].split(",")
-    print(state)
     while days != 0:
+        print(days)
         temp = list()
-        extras = 0
         for each in state:
             if int(each) > 0:
                 temp.append(int(each) - 1)
             else:
                 temp.append(6)
-                extras += 1
-        for each in range(extras):
-            temp.append(8)
+                temp.append(8)
         state = temp
         days -= 1
     return len(state)
 
-print(part1(80))
+def part2(days):
+    fish_map = mapcreator(data[0].split(','))
+    for _ in range(days):
+        temp_fish_map= dict()
+        for each in range(9):
+            temp_fish_map[each] = 0
+        for timer, fishes in fish_map.items():
+            timer -= 1
+            if int(timer) < 0:
+                temp_fish_map[8] = fishes
+                temp_fish_map[6] += fishes
+            else:
+                temp_fish_map[int(timer)] += fishes
+        fish_map = temp_fish_map
+    return sum(fish_map.values())
+
+
+def mapcreator(fishies):
+    retval = dict()
+    for each in range(9):
+        retval[each] = 0
+    for each in fishies:
+        retval[int(each)] += 1
+    return retval
+
+
+#print(part1(80))
+print(part2(256))
