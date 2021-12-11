c6da4facba0f3b302a1b349a751b60d8b1188984
Vatsav Sethupathi <vatsav14@outlook.com>
2021-12-11 04:33:14 -0700

    Day 5 part 2

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -6,8 +6,6 @@ for each in data:
     coord.append([(each[0].split(',')[0],each[0].split(',')[1]), \
                   (each[2].split(',')[0],each[2].split(',')[1])])
 
-print(coord)
-
 board = list()
 for each in range(1000):
     board.append([0] * 1000)
@@ -30,7 +28,25 @@ def part1():
                 board[x1][int(each[0][1])] += 1
                 x1 += 1
 
+        elif abs(int(each[0][0]) - int(each[1][0])) == abs(int(each[0][1]) - int(each[1][1])):
+            x1, x2, y1, y2 = int(each[0][0]), int(each[1][0]), int(each[0][1]), int(each[1][1])
+            if x1 < x2:
+                x = 1
+            else:
+                x = -1
+            if y1 < y2:
+                y = 1
+            else:
+                y = -1
+            while x1 - x2 != 0:
+                board[x1][y1] += 1
+                x1 += x
+                y1 += y
+            # To make sure the last point is also accounted for
+            board[x1][y1] += 1
+        
 part1()
+
 
 total = 0
 for each in board:
